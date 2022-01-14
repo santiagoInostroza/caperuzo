7b090f83d60ef46289209de0d85983119a0b9cb1
Santiago Inostroza <santiagoinostroza2@gmail.com>
2022-01-14 16:27:15 -0300

    ajustes

--- resources/views/livewire/home/index.blade.php
+++ resources/views/livewire/home/index.blade.php
@@ -34,7 +34,7 @@
     </div>
 
 
-    <div class="max-w-7xl m-auto px-8 md:px-16 xl:px-0">
+    <div class="max-w-7xl m-auto px-8 md:px-16 lg:px-32">
         {{-- CELULAR --}}
         <div>
             <div class="flex w-full items-center justify-center lg:hidden  p-4 text-white text-2xl shadow rounded-xl ">
@@ -68,11 +68,11 @@
 
         
 
-        <div class="py-16 flex flex-wrap gap-2 gap-y-20 md:gap-y-44 justify-around px-8">       
-            <img class="object-cover w-1/4  transform skew-y-6 skew-x-2 transition-all hover:scale-200 lg:hover:scale-175 duration-700 hover:skew-y-0 hover:skew-x-0 cursor-pointer shadow-2xl " src="{{asset('images/banda/5258.jpg')}}" alt=""> 
-            <img class="object-cover w-1/4  transform skew-y-6 skew-x-2 transition-all hover:scale-200 lg:hover:scale-175 duration-700 hover:skew-y-0 hover:skew-x-0 cursor-pointer shadow-2xl" src="{{asset('images/banda/4817.jpg')}}" alt=""> 
-            <img class="object-cover w-1/4  transform skew-y-6 skew-x-2 transition-all hover:scale-200 lg:hover:scale-175 duration-700 hover:skew-y-0 hover:skew-x-0 cursor-pointer shadow-2xl" src="{{asset('images/banda/5212.jpg')}}" alt=""> 
-            <img class="object-cover w-3/4  transform skew-y-6 skew-x-2 transition-all duration-700 hover:skew-y-0 hover:skew-x-0 hover:scale-150 cursor-pointer shadow-2xl " src="{{asset('images/banda/banda_caperuzzo_juntos.jpg')}}" alt="">
+        <div class="py-16 flex flex-wrap gap-2 gap-y-20 md:gap-y-44 justify-between px-8">       
+            <img class="object-cover w-1/4 transform skew-y-6 skew-x-2 transition-all hover:scale-200 lg:hover:scale-175 duration-700 hover:skew-y-0 hover:skew-x-0 cursor-pointer shadow-2xl" src="{{asset('images/banda/5258.jpg')}}" alt=""> 
+            <img class="object-cover w-1/4 transform skew-y-6 skew-x-2 transition-all hover:scale-200 lg:hover:scale-175 duration-700 hover:skew-y-0 hover:skew-x-0 cursor-pointer shadow-2xl" src="{{asset('images/banda/4817.jpg')}}" alt=""> 
+            <img class="object-cover w-1/4 transform skew-y-6 skew-x-2 transition-all hover:scale-200 lg:hover:scale-175 duration-700 hover:skew-y-0 hover:skew-x-0 cursor-pointer shadow-2xl" src="{{asset('images/banda/5212.jpg')}}" alt=""> 
+            <img class="object-cover w-4/4 transform skew-y-6 skew-x-2 transition-all duration-700 hover:skew-y-0 hover:skew-x-0 hover:scale-125 cursor-pointer shadow-2xl " src="{{asset('images/banda/banda_caperuzzo_juntos.jpg')}}" alt="">
             <img class="object-cover hover:object-contain w-4/4 transform skew-y-6 skew-x-2 transition-all duration-700 hover:skew-y-0 hover:skew-x-0 hover:scale-125 cursor-pointer shadow-2xl" src="{{asset('images/banda/banda_caperuzzo.jpg')}}" alt="">
         </div>
         <div class="w-full relative border-white my-12">
